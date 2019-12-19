33f2e6412e01bbdf7159d5860a7ba97ce8ba19b8
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2019-12-19 14:45:17 -0500

    Delete Models.py

--- Models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-#Creating the models
-class Contract(models.Model):
-    name = models.CharField(max_length=200)
-    doorNo = models.SlugField(max_length=200)
-    Address = models.TextField(black=True)
