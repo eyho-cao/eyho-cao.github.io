705b4ca0b302445647ce8c8b967ae8fd35d66b08
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2019-12-19 14:45:02 -0500

    Delete Forms.py

--- Forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from contracts.models import Contract
-
-class GenerateContract(forms.ModelForm):
-    class Meta():
-        model = Contract
-        fields = '__all__'
-        
